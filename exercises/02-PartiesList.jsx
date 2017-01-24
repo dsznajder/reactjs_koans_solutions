0a7d914f1abe909985c80412a8a69740fcce2a3d
dsznajder <damsznajder@gmail.com>
2017-01-24 08:56:02 +0100

    exercise 2

---
+++ exercises/02-PartiesList.jsx
@@ -0,0 +1,14 @@
+var React = require("react");
+
+class PartiesList extends React.Component {
+  render() {
+    return (
+      <ul className="parties-list">
+        <li>Party at Aperture Laboratories</li>
+        <li>Party at Aperture Laboratories</li>
+      </ul>
+    );
+  }
+}
+
+export default PartiesList;
