9c32774c44e06c9fcfd195179febec542bd5239c
dsznajder <damsznajder@gmail.com>
2017-01-24 08:56:37 +0100

    exercise 5, part 1

---
+++ exercises/05-Challenge-GroceryList-part-1.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groceries: [ { name: "Apples" } ]
+    };
+  }
+
+  render() {
+    let groceriesComponents = [];
+
+    for(var index = 0; index < this.state.groceries.length; index++) {
+      groceriesComponents.push(
+          <GroceryListItem
+            grocery={this.state.groceries[index]}
+          />
+      );
+    }
+
+    return (
+      <div>
+        <ul>
+          {groceriesComponents}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <li>
+          {this.props.grocery.name}
+        </li>
+    );
+  }
+}
+
+export default GroceryList;
